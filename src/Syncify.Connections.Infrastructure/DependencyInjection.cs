48702becb92daf445c5700f1530c0d0325cd06a9
Iryna Velmakina <i.velmakina@headway.inc>
2026-04-05 19:41:20 +0300

    feat: add connection service facade implementatio

--- src/Syncify.Connections.Infrastructure/DependencyInjection.cs
+++ src/Syncify.Connections.Infrastructure/DependencyInjection.cs
@@ -6,6 +6,7 @@ using Syncify.Connections.Infrastructure.Google;
 using Syncify.Connections.Infrastructure.Persistence;
 using Syncify.Connections.Infrastructure.Security;
 using Syncify.Connections.Infrastructure.Services;
+using Syncify.Shared;
 
 namespace Syncify.Connections.Infrastructure;
 
@@ -15,12 +16,15 @@ public static class DependencyInjection
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        services.Configure<GoogleOptions>(configuration.GetSection(GoogleOptions.SectionName));
+        services.Configure<EncryptionOptions>(configuration.GetSection(EncryptionOptions.SectionName));
+
         services.AddDbContext<ConnectionsDbContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
         services.AddScoped<ICalendarAccountRepository, CalendarAccountRepository>();
         services.AddScoped<ITokenEncryptor, TokenEncryptor>();
-        services.AddScoped<Syncify.Shared.IConnectionService, ConnectionService>();
+        services.AddScoped<IConnectionService, ConnectionService>();
 
         services.AddHttpClient<IOAuthProvider, GoogleOAuthProvider>();
         services.AddHttpClient<ICalendarProvider, GoogleCalendarProvider>();
