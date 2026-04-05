38f604b8227fe614f71c7b2155848489c503a119
Iryna Velmakina <i.velmakina@headway.inc>
2026-04-05 18:02:01 +0300

    feat: add configurations

---
+++ src/Syncify.Connections.Infrastructure/Persistence/Configurations/CalendarAccountConfiguration.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Syncify.Connections.Infrastructure.Persistence.Entities;
+
+namespace Syncify.Connections.Infrastructure.Persistence.Configurations;
+
+public class CalendarAccountConfiguration : IEntityTypeConfiguration<CalendarAccountEntity>
+{
+    public void Configure(EntityTypeBuilder<CalendarAccountEntity> builder)
+    {
+        builder.ToTable("calendar_accounts");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedNever();
+
+        builder.Property(x => x.UserId)
+            .HasColumnName("user_id")
+            .IsRequired();
+
+        builder.Property(x => x.Provider)
+            .HasColumnName("provider")
+            .IsRequired();
+
+        builder.Property(x => x.RefreshTokenEnc)
+            .HasColumnName("refresh_token_enc")
+            .IsRequired();
+
+        builder.Property(x => x.TokenExpiresAt)
+            .HasColumnName("token_expires_at")
+            .IsRequired();
+
+        builder.Property(x => x.Status)
+            .HasColumnName("status")
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+
+        builder.Property(x => x.UpdatedAt)
+            .HasColumnName("updated_at")
+            .IsRequired();
+
+        builder.HasMany(x => x.Calendars)
+            .WithOne(c => c.Account)
+            .HasForeignKey(c => c.AccountId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
